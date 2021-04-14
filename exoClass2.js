ce1bee55cb37fb2b8bde9519f99d22ee90e88957
antoine <nogoup.antoine@gmail.com>
2021-04-14 22:22:22 +0200

    next commit

--- exoClass2.js
+++ exoClass2.js
@@ -5,13 +5,24 @@ class Point {
         this.x = x
     }
 
-getY(){
+get Yaxis(){
     return `${this.y}`
 }
 
-getX(){
-    return `${this.x}`
+get Xaxis(){
+    return `${this.x}`;
 }
     
+set Yaxis(y){
+   return this.y;
+}
+
+set Xaxis(x){
+    return this.x;
+ }
 
 }
+
+let user = new Point('john', 'antoine')
+
+console.log(user.Yaxis)
--- exoclass1.js
+++ exoclass1.js
@@ -19,3 +19,11 @@ class Hippopotamus {
         return `Name: ${this.name} : weight: ${this.weight } tusksSize: ${this.tusksSize}`  
     }
 }
+
+let hippo = new Hippopotamus('thomas', 84, 18)
+
+console.log(hippo.swim())
+
+console.log(hippo.eat())
+
+console.log(hippo.toString())
