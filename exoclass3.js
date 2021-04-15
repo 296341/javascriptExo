1b99af93169d35c1ca411aec2c4a9aad53bae1a6
antoine <nogoup.antoine@gmail.com>
2021-04-16 00:38:02 +0200

    html first commit

--- exoclass3.js
+++ exoclass3.js
@@ -1,6 +1,6 @@
 const Point = require("./exoClass2");
 
-var points = new Point(6, 7);
+var points = new Point(8, 4);
 
 class Circle extends Point{
 
@@ -16,14 +16,14 @@ class Circle extends Point{
   }
 
   containPoint(){
-    let diffx = points.Xaxis - this.point1
-    let diffy = points.Yaxis - this.point2
+    const diffx = points.Xaxis - this.point1
+    const diffy = points.Yaxis - this.point2
 
     console.log(`diffy :${diffy}`)
     console.log(`diffx :${diffx}`)
 
     
-    let longueur = Math.sqrt((diffx * diffx)+(diffy * diffy))
+    const longueur = Math.sqrt((diffx * diffx)+(diffy * diffy))
     console.log(`longueur : ${longueur}`)
 
     if(longueur<= this.raduis && longueur >= 0){
@@ -32,12 +32,18 @@ class Circle extends Point{
         console.log('false')
     }
   }
+
+  toString(){
+    return `x1: ${this.point1} y1: ${this.point2 } raduis: ${this.raduis}`  
+}
 }
 
 
 
-let c = new Circle(1,2,7)
+let c = new Circle(3,2,7)
 
-console.log(c.containPoint())
+console.log(`${c.containPoint()}`)
 
 console.log(`the area is : ${c.areaCircle()}`)
+
+console.log(c.toString())
---
+++ exohtmlscript.js
@@ -0,0 +1,41 @@
+window.addEventListener("load", function () {
+    
+    let element = document.querySelector("button");
+
+
+    var paragraph = document.getElementsByTagName("body")[0];
+    var para = document.createElement("p");
+    var text = document.createTextNode("This is a new paragraph.");
+    para.appendChild(text); 
+    
+
+
+    function myFun(){
+        let randomcolor= "";
+        let allchar="0123456789ABCDEF";
+        for(let i=0; i<6; i++){
+           randomcolor += allchar[Math.floor(Math.random()*16)];
+           paragraph.appendChild(para)
+        }
+         document.body.style.backgroundColor= "#"+randomcolor;
+   
+    }
+
+
+   
+
+    function myJquery(){
+
+        let randomColor = '#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
+        
+        $('body').css({
+          'background-color' : randomColor,
+        });
+    
+     } 
+
+
+
+
+      element.addEventListener("click", myJquery)
+})
